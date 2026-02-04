c1b4018f3593b27bd18d11effa98616bb10be3dc
geecynthia3-glitch <geecynthia3@gmail.com>
2026-02-03 23:41:31 -0500

    Add logging for CLICKUP_API_TOKEN length
    Log the length of CLICKUP_API_TOKEN for debugging.

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const axios = require("axios");
 console.log("CLICKUP_API_TOKEN exists?", !!process.env.CLICKUP_API_TOKEN);
 console.log("CLICKUP_LIST_ID exists?", !!process.env.CLICKUP_LIST_ID);
 console.log("WEBHOOK_SECRET exists?", !!process.env.WEBHOOK_SECRET);
+console.log("CLICKUP_API_TOKEN length:", process.env.CLICKUP_API_TOKEN?.length);
+
 
 const app = express();
 app.use(express.json());
